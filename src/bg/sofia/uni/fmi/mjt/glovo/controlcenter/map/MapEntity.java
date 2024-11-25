e0f0b35d801cb053a0fbf5369f6e5e4eaaa9885d
Boris Yasin <109368863+BoRKaTa2111@users.noreply.github.com>
2024-11-25 21:50:57 +0200

    hw 1 - skeleton

---
+++ src/bg/sofia/uni/fmi/mjt/glovo/controlcenter/map/MapEntity.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.glovo.controlcenter.map;
+
+public class MapEntity {
+
+    public MapEntity(Location location, MapEntityType type) {
+    }
+}
---
+++ src/bg/sofia/uni/fmi/mjt/glovo/exception/InvalidOrderException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.glovo.exception;
+
+public class InvalidOrderException extends RuntimeException {
+    InvalidOrderException(String s) {
+        super(s);
+    }
+}
